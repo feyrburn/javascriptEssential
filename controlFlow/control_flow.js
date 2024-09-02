49b9d7a559213016b6fcb1454a032964ca8f1d28
feyrburn <s_h_o_g_u_n2003@yahoo.com>
2024-09-02 01:46:59 -0400

    additional logic

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -49,3 +49,23 @@ let isAuthenticated = true;
 let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
 
 console.log("Authenticatio Status:", authenticationStatus);
+
+let memberType = "Enrolled Member";
+let memberContentAccess;
+
+switch(memberType) {
+    case "Employee":
+        memberContentAccess = "Dietary Services";
+        break;
+    case "Enrolled Member":
+        memberContentAccess = "Dietary Services and one-on-one interaction with a dietician";
+        break;
+    case "Subscriber":
+        memberContentAccess = "Partial Access to facilitate Dietary Services";
+        break;
+    default:
+        memberContentAccess = "Do not have access, please subsribe/enrolled to the program to enjoy access";
+}
+
+console.log("Member type:", memberType);
+console.log("Member access:", memberContentAccess);
