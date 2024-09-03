477ab01243fa8d27e52a6467d152d29beee8397c
feyrburn <s_h_o_g_u_n2003@yahoo.com>
2024-09-02 23:31:27 -0400

    add user experience

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -6,6 +6,7 @@ function submitFeedback(){
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const exp = document.getElementById('userExperience').value;
     
     alert("Thank you for your valuable feedback");
 
@@ -16,6 +17,8 @@ function submitFeedback(){
     document.getElementById("userDesignation").innerHTML = designation;
     document.getElementById("userProductChoice").innerHTML = productType;
     document.getElementById("userFeedback").innerHTML = feedback;
+    document.getElementById("userExperienceDisplay").innerHTML = exp;
+    document.getElementById("userProductChoice2").innerHTML = productType;
     document.getElementById("userInfo").style.display = "block";
 }
 
